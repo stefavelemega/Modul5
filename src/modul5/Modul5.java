c5f75bacb09d315c4c882ceb0f30bd69c57cf590
stefavelmega <38370417+stefavelmega@users.noreply.github.com>
2018-04-23 12:05:03 +0700

    Ini tugas modul6

---
+++ src/modul5/Modul5.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modul5;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class Modul5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+          input pr = new input();
+          pr.kel();
+          pr.print();
+    }
+    
+}
