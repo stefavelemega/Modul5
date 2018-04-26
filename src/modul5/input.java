80e5f6e439dd070a3e7b9ef58aa0ad31d7671ecc
OryzaSatifha <38368454+OryzaSatifha@users.noreply.github.com>
2018-04-26 11:27:11 +0700

    Update input.java

--- src/modul5/input.java
+++ src/modul5/input.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//haymegacantik
+//inioryzcute
 package modul5;
 
 import java.util.Scanner;
-
-/**
- *
- * @author WINDOWS 10
- */
 
 public final class input{
     public float NIM;
